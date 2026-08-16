72a8d10459d7c486cceb1527d8c44873eadb4dcc
vaibhavsharma <vaibhav53091@gmail.com>
2026-08-16 22:05:54 +0530

    completed polymorphism and its examples

---
+++ 13_OOPS/04_Polymorphism/01_def_polymorphism.py
@@ -0,0 +1,80 @@
+#------------------------------------------------------------------------|   
+                   # What is Polymorphism? (English)
+#------------------------------------------------------------------------|
+
+# Definition:
+
+# Polymorphism means "Many Forms."
+# In Python, one method, function, or operator can perform different tasks depending on the object or data type.
+
+# SIMPLE DEFINATION
+
+# One interface, many implementations.
+# It means the same method name can behave differently for different objects.
+
+# Polymorphism क्या है? (Hindi)
+
+# परिभाषा:
+
+# Polymorphism का मतलब होता है "एक चीज़ के कई रूप"।
+
+# Python में एक ही function, method या operator अलग-अलग objects के साथ अलग-अलग तरीके से काम करता है।
+
+# आसान भाषा में
+
+# एक नाम, कई काम।
+
+# यानी एक ही Method अलग-अलग object के लिए अलग-अलग Result देता है।
+
+
+#----------------------------------------------------------------|
+                # Why Do We Use Polymorphism?
+#----------------------------------------------------------------|
+
+# Polymorphism helps us:
+
+# Write reusable code
+# Reduce duplicate code
+# Make programs flexible
+# Improve readability
+# Easier maintenance
+# Hindi
+
+# Polymorphism का उपयोग इसलिए करते हैं क्योंकि:
+
+# Code दोबारा लिखने की जरूरत नहीं पड़ती
+# Program Flexible बनता है
+# Code साफ और छोटा होता है
+# Maintenance आसान होती है
+
+# --------------------------------------------------------------------------|
+                          # Advantages of Polymorphism
+# --------------------------------------------------------------------------|
+# Code Reusability
+# Flexibility
+# Less Code
+# Easy Maintenance
+# Better Readability
+# Easier Extension
+
+# Hindi
+# Code Reuse होता है
+# Program Flexible बनता है
+# कम Code लिखना पड़ता है
+# Maintenance आसान होती है
+# Readability बढ़ती है
+# Future में Features जोड़ना आसान होता है
+
+
+# --------------------------------------------------------------------------|
+                          # Disadvantages of Polymorphism
+# --------------------------------------------------------------------------|
+
+# Can be confusing for beginners
+# Harder to debug in complex projects
+# Too much polymorphism may reduce readability
+
+# Hindi
+# Beginners के लिए समझना थोड़ा कठिन हो सकता है
+# बड़े Projects में Debug करना मुश्किल हो सकता है
+# ज्यादा Polymorphism से Code समझना कठिन हो सकता है
---
+++ 13_OOPS/04_Polymorphism/02_example.py
@@ -0,0 +1,54 @@
+#Example 1 -------->>>>>>>>>>>>>>>
+
+#SAME CLASSES,DIFFERENT DATA TYPES
+#INBUILT  FUNCTION
+
+print(len("python"))
+print(len([10,20,30]))
+print(len((1,2,3,4)))
+
+
+# Example 2 -------->>>>>>>
+
+#DIFFERENT CLASSES,SAME METHOD
+
+class dog :
+    def sound(self):
+        print("dog barks")
+        
+class cat:
+    def sounf(self):
+        print("cat meows")
+        
+        
+d = dog()
+c = cat()
+
+d.sound()
+c.sound()
+
+ # EXAMPLE 3 -------------------->>>>>>>   
+        
+#loop with multiple objects
+
+class Dog:
+    def sound(self):
+        print("Bark")
+
+class Cat:
+    def sound(self):
+        print("Meow")
+
+class Cow:
+    def sound(self):
+        print("Moo")
+
+animals = [Dog(), cat(), Cow()]
+
+for animal in animals:
+    animal.sound()
+
+#English
+
+#The same sound() method is called for different objects, and each object responds in its own way.
+
