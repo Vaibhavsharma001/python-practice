0a879b4c2f4a6d6a6236063cc55048b65e833226
vaibhavsharma <vaibhav53091@gmail.com>
2026-08-30 10:23:30 +0530

    added functools defination

---
+++ 16_functools/01_def.py
@@ -0,0 +1,15 @@
+# 🐍 What is functools?
+
+# functools is a built-in Python module that gives us tools for working with functions.
+
+# Think of it like:
+
+# functools = a toolbox for functions 🧰
+
+#  For advanced Python, focus mainly on these:
+
+# functools.wraps() 
+# functools.reduce()
+# functools.partial()
+# functools.lru_cache()
+# functools.cache()
